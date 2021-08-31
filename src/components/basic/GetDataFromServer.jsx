0abeeb06bfc4bdaeef299892591409aac817685b
Abu Rayhan <rayhan.cse15@gmail.com>
2021-08-31 19:30:42 +0600

    Get API Call done

---
+++ src/components/basic/GetDataFromServer.jsx
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import React from 'react'
+import { useState, useEffect  } from 'react'
+
+export default function GetDataFromServer() {
+
+    const [weathers, setweathers] = useState([])
+
+    useEffect(() => {
+        axios.get("https://localhost:44344/weatherforecast").then(res=>{
+            setweathers(res.data);
+        })
+    }, [])
+
+    console.log("weathers",weathers)
+
+    return (
+        <div style="width:40%">
+            <ul>
+                {weathers.map((weather) =>{
+                    return <li key={weather.temperatureC}>{weather.temperatureC}</li>
+                })}
+            </ul>
+        </div>
+    )
+}
+
+
+// export default GetDataFromServer; 
